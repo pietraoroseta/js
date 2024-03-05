bc8aea9f606a95de2d068b024fe3cafa1792325e
pietraoroseta <143437566+pietraoroseta@users.noreply.github.com>
2024-03-05 17:04:12 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// boolean
+// true -> verdade 
+// false -> falso
+
+const primeironumero = 5;
+const segundonumero = 10;
+const cadastroativado = true;
+
+//console.log(primeironumero === segundonumero) ;
+
+const texto1 = "alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2)
